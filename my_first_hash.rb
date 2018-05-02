a252afd6c71a2a12f5fe5664122988fa9a6ce771
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-02 19:45:50 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,9 +17,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+  shipping_manifest["oil_painting"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
